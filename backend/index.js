175448c5c786e5da54b5ca88a97e08d81c1479b5
Marcos Nobre <nobremarcos95@gmail.com>
2024-12-11 01:06:38 -0300

    fix CORS on server

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 import User from './models/User.js';
 
 const app = express();
 
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5500',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:5500',
+  ]
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {});
@@ -39,7 +48,7 @@ app.post('/login', async (req, res) => {
   const { username = '', password = '' } = req.body;
 
   const result = await User.find({ username, password }).exec();
-  return res.json(!!result.length);
+  return res.json(result);
 });
 
 app.delete('/delete-all', async (req, res) => {
